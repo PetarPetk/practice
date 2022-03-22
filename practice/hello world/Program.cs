3024b21c8b1d80ac88a6cd9728ec0b8a631e953b
petarpetk <vishi.gospodin.petar@live.com>
2022-03-22 17:45:47 +0100

    Testing Commits

--- practice/hello world/Program.cs
+++ practice/hello world/Program.cs
@@ -1,3 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Test123");
+﻿using System;
+
+namespace PracticeStuff
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("YO");
+            Console.WriteLine("Sup!");
+            Console.Beep();
+        
+        }
+    }
+}
+
